41cebbf861cdbdabc03cb9445b797843362a0539
Alex Bahm <alexgbahm@gmail.com>
2015-01-03 16:23:19 -0800

    Broke up the removeHiddenTeams function for readability. I converted tabs to spaces. Finally, I added the ability to specify the image extension

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 var path = require('path'),
-	http = require('http'),
-	express = require('express'),
-	bodyParser = require('body-parser'),
-	config = require(path.join(__dirname, 'config.json')),
-	compression = require('compression'),
-	app = express(),
-	request = require('request'),
-	async = require('async'),
-	_teams = [],
-	fs = require('fs'),
-	glob = require('glob');
+  http = require('http'),
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  config = require(path.join(__dirname, 'config.json')),
+  compression = require('compression'),
+  app = express(),
+  request = require('request'),
+  async = require('async'),
+  _teams = [],
+  fs = require('fs'),
+  glob = require('glob');
 
 
 app.set('port', process.env.VCAP_APP_PORT || 80);
@@ -23,205 +23,225 @@ app.get("/imgs/:year?", getImageNames);
 
 var server = http.createServer(app);
 
+/************************************** API End Points **************************************/
+
 function getImageNames(req, res) {
-	var year = req.param('year', null);
-	var img_path = path.join("web", "images");
-	
-	if(year) {
-		img_path = path.join(img_path, year);
-	}
+  var year = req.param('year', null);
+  var file_ext = req.param('ext', 'jpg') //default parameter is jpg
+  var img_path = path.join('web', 'images');
+  
+  if(year) {
+    img_path = path.join(img_path, year);
+  }
 
-	img_path = path.join(img_path, "**", "*.jpg"); //for globbing
+  img_path = path.join(img_path, "**", "*." + file_ext); //for globbing
 
-	glob(img_path, function(err, names) {
-			console.log("Files: " + JSON.stringify(names));
-		res.send(names);
-	});
+  glob(img_path, function(err, names) {
+      console.log("Files: " + JSON.stringify(names));
+    res.send(names);
+  });
 }
 
 function getTeamInfo(req, res) {
-	var processed = [];
-
-	_teams.forEach(function(team) {
-		var teamObj = {
-			name: team.name,
-			members: team.attendee_count,
-			description: team.description,
-			captain: team.captain
-		};
-		processed.push(teamObj);
-	});
-
-	res.send(processed);
+  res.send(_teams);
 }
 
+/************************************** Team Processing Functions **************************************/
+
+//Happened more commonly in old versions. 
 function removeDuplicates(list) {
-	var uniqueArr = list.filter(function(elem, pos) {
-		return list.indexOf(elem) == pos;
-	});
-	return uniqueArr;
+  var uniqueArr = list.filter(function(elem, pos) {
+    return list.indexOf(elem) == pos;
+  });
+  return uniqueArr;
+}
+
+function processResponse(err, team, creator) {
+  if(creator) {
+    var response = getUserResponse(creator.answers);
+
+    if(response.approved) {
+      var teamObj = {
+        name: team.name,
+        members: team.attendee_count,
+        description: response.description,
+        captain: {
+          name: creator.profile.name,
+          email: creator.profile.email
+        }
+      }
+      _teams.push(teamObj);
+    }
+  }
+}
+
+function getCreator(team, attendees, cb) {
+  attendees.forEach(function(attendee) {
+    if(team.creator.emails[0].email === attendee.profile.email) {
+      cb(null, team, attendee);
+    }
+  });
+  cb(null);
+}
+
+function getUserResponse(questions) {
+  var response = {
+    approved: false,
+    description: null
+  };
+
+  questions.forEach(function(answer) {
+    if(answer.question_id === "8622569") { //"question": "Would you like to have your team listed on our Team finder page?",
+      if(answer.answer === "Yes, I am looking for additional members") {
+        response.approved = true;
+      }
+
+    } else if(answer.question_id === "8622571") {
+      if(typeof(answer.answer) !== "undefined") {
+        response.description = answer.answer;
+      }
+    }
+  });
+
+  return response;
 }
 
 function removeHiddenTeams(teams, attendees) {
-	var visible_teams = [],
-		answered = false,
-		approved = false,
-		description = null,
-		captain = null;
+  var visible_teams = [],
+    answered = false,
+    approved = false,
+    description = null,
+    captain = null;
 
-	teams.forEach(function(team, i_team) {
-		attendees.forEach(function(user, i) {
-			answered = false;
-			approved = false;
-			description = null;
-			captain = null;
-			
-			if(team.creator.emails[0].email === user.profile.email) {
-				captain = {
-					name: user.profile.name,
-					email: user.profile.email
-				};
+  for(var i = 0; i < teams.length; i++) {
+    var team = teams[i];
+    getCreator(team, attendees, processResponse);
+  }
 
-				user.answers.forEach(function(question) {
-					if(question.question_id === "8622569") { //"question": "Would you like to have your team listed on our Team finder page?",
-						if(question.answer === "Yes, I am looking for additional members") {
-							approved = true;							
-						}
+  _teams = removeDuplicates(_teams); //ensure it is free of dups
 
-					} else if(question.question_id === "8622571") {
-						if(typeof(question.answer) !== "undefined") {
-							description = question.answer;
-						}
-					}
-				});
-
-				if(approved) {
-					team.description = description;
-					team.captain = captain;
-					_teams.push(team);
-				}
-			}
-		});
-	});
-
-	_teams = removeDuplicates(_teams); //ensure it is free of dups
-
-	return;
+  return;
 }
 
+//removes small teams
 function filterTeams(teams) {
-	var filtered = [];
-	teams.forEach(function(team) {
-		if(team.attendee_count < 3) {
-			filtered.push(team);
-		}
-	});
-	//console.log(filtered.length + " teams have < 3 people on them");
-	return filtered;
+  var filtered = [];
+  teams.forEach(function(team) {
+    if(team.attendee_count < 3) {
+      filtered.push(team);
+    }
+  });
+  //console.log(filtered.length + " teams have < 3 people on them");
+  return filtered;
 }
 
+/************************************** EVENTBRITE API Requests **************************************/
 var requestTeams = function(callback) {
-	var request_url = "https://www.eventbriteapi.com/v3/events/" + config.eventbrite.event_id + "/teams/?token=" + config.eventbrite.oathtoken;
-	make_request(request_url, callback);
-}
+  var request_url = "https://www.eventbriteapi.com/v3/events/" + config.eventbrite.event_id + "/teams/?token=" + config.eventbrite.oathtoken;
+  make_request(request_url, callback);
+};
 
 var requestAttendees = function(callback) {
-	var request_url = "https://www.eventbriteapi.com/v3/events/" + config.eventbrite.event_id + "/attendees/?token=" + config.eventbrite.oathtoken;
-	make_request(request_url, callback);
-}
+  var request_url = "https://www.eventbriteapi.com/v3/events/" + config.eventbrite.event_id + "/attendees/?token=" + config.eventbrite.oathtoken;
+  make_request(request_url, callback);
+};
 
 function make_request(url, callback) {
-	function actual_request(url, callback) {
-		request(url, function(err, response, body) {
-			if(!err && response.statusCode === 200) {
-				callback(err, JSON.parse(body));
-			} else {
-				console.log("Error: " + JSON.stringify(err));
-				console.log("response code: " + JSON.stringify(response.statusCode));
-				callback(err);
-			}
-		});	
-	}
+  function actual_request(url, callback) {
+    request(url, function(err, response, body) {
+      if(!err && response.statusCode === 200) {
+        callback(err, JSON.parse(body));
+      } else {
+        console.log("Error: " + JSON.stringify(err));
+        console.log("response code: " + JSON.stringify(response.statusCode));
+        callback(err);
+      }
+    }); 
+  }
 
-	function page(url, field, callback) {
-		actual_request(url, function(err, body) {
-			if(err) {
-				callback(err, results); //return the results we already have
-			}
-			callback(err, body[field]);
-		});
-	}
+  //To assist with the pagination of results
+  function page(url, field, callback) {
+    actual_request(url, function(err, body) {
+      if(err) {
+        callback(err, results); //return the results we already have
+      }
+      callback(err, body[field]);
+    });
+  }
 
-	actual_request(url, function(err, body) {
-		if(err) {
-			callback(err); 
-		}
+  actual_request(url, function(err, body) {
+    if(err) {
+      callback(err); 
+    }
 
-		var field = Object.keys(body)[1]; //gets the field after pagination
-		if(body.pagination.page_number < body.pagination.page_count) {
-			var results = body[field];
-			var tasks = [];
+    var field = Object.keys(body)[1]; //gets the field after pagination
+    if(body.pagination.page_number < body.pagination.page_count) {
+      var results = body[field];
+      var tasks = [];
 
-			for(var i = 2; i <= body.pagination.page_count; i++) {
-				var pageURL = url + '&page=' + i;
-				tasks.push(async.apply(page, pageURL, field))
-			}
+      for(var i = 2; i <= body.pagination.page_count; i++) {
+        var pageURL = url + '&page=' + i;
+        tasks.push(async.apply(page, pageURL, field));
+      }
 
-			async.parallel(tasks, function(err, _results) {
+      async.parallel(tasks, function(err, _results) {
 
-				_results.forEach(function(result) {
-					results = results.concat(result);
-				});
+        _results.forEach(function(result) {
+          results = results.concat(result);
+        });
 
-				callback(err, results);
-			});
-		} else {
-			callback(err, body[field]);
-		}
-	});	
+        callback(err, results);
+      });
+    } else {
+      callback(err, body[field]);
+    }
+  }); 
 }
 
 var processResults = function(err, results) {
-	if(!err) {
-		var attendees = results[0];
-		var teams = results[1];
-		console.log("Total attendees: " + attendees.length);
+  if(!err) {
+    var attendees = results[0];
+    var teams = results[1];
+    console.log("Total attendees: " + attendees.length);
 
-		teams = filterTeams(teams);
-		
-		removeHiddenTeams(teams, attendees);
+    teams = filterTeams(teams);
+    
+    removeHiddenTeams(teams, attendees);
 
-		console.log("Teams that need members: " + _teams.length);
+    console.log("Teams that need members: " + _teams.length);
 
-	} else {
-		console.log("Error getting results: " + JSON.stringify(err));
-	}
-}
+  } else {
+    console.log("Error getting results: " + JSON.stringify(err));
+  }
+};
+
 
 function update() {
-	var now = new Date();
-	console.log("Updating at " + now.toLocaleString());
-	async.parallel([
-		requestAttendees,
-		requestTeams
-	],
-	processResults);
+  var now = new Date();
+  console.log("Updating at " + now.toLocaleString());
+  async.parallel([
+    requestAttendees,
+    requestTeams
+  ],
+  processResults);
 }
 
+// Log errors before crash
 app.use(function(err, req, res, next) {
-	if(!err) return next();
-	console.log("Error: ".red + JSON.stringify(err));
-	console.log(err.stack);
-	if(!res.headersSent) {
-		res.json({error: err});
-	}
+  if(!err) return next();
+  console.log("Error: ".red + JSON.stringify(err));
+  console.log(err.stack);
+  if(!res.headersSent) {
+    res.json({error: err});
+  }
 });
 
+//Start server
 server.listen(app.get('port'), function(){
-	console.log('Express server listening on port ' + app.get('port'));
-	update();
+  console.log('Express server listening on port ' + app.get('port'));
+  update();
 });
 
 var updateInterval = setInterval(function() {
-	update();
+  update();
 }, 1000 * 60 * 20); //update every 20 minutes
