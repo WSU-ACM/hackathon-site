715eccb8ab08718f2992a17ec75c8071f9824ea3
Dylan Powers <dylan.kyle.powers@gmail.com>
2015-02-11 22:08:31 -0800

    deploy: script updated to work with jekyll

--- deploy.sh
+++ deploy.sh
@@ -1,54 +1,70 @@
 #!/bin/bash
 
 deploy_dir=/var/www/hackathon-site
-stage_dir=hackathon-site-stage
+build_tarball=hackathon-site-build.tar
 
 server_addr=hackathon.eecs.wsu.edu
 ssh_port=11993
 
+clean_up() {
+  if [ -e $build_tarball ]; then rm $build_tarball; fi
+}
+
 failure() {
   echo
   echo "Deployment failed :("
+  clean_up
   exit
 }
+trap failure SIGHUP SIGINT SIGTERM
 
-rm -rf build
-
-gulp build
-if [ $? -ne 0 ]; then
-  failure
-fi
+jekyll build
+if [ $? -ne 0 ]; then failure; fi
 
 echo
 read -p "Enter your user name on the server: " user_name
 
-echo "Attempting to clean the staging directory. If this fails it's okay"
-ssh -p $ssh_port $user_name@$server_addr rm -r $stage_dir
+
 echo
 echo 'Copying to the server...'
-scp -P $ssh_port -r build/. $user_name@$server_addr:$stage_dir
-if [ $? -ne 0 ]; then
-  failure
-fi
-echo
 
-# The scary part! Move the staging directory to the deploy directory. There
-# are quite a few ways of doing this so that it's an atomic operation, but
-# this is the simplest and serves our purposes.
-echo 'Deploying...'
-ssh -p $ssh_port $user_name@$server_addr \
-  "rm -r $deploy_dir && \
-  mv $stage_dir $deploy_dir && \
-  chown -R :web $deploy_dir && \
-  chmod -R o-w $deploy_dir && \
-  chmod -R g+w $deploy_dir"
+## Creating a tar archive. No need for compression however. Just need to
+## reduce the number of files transferred.
+tar -cf $build_tarball build
 
-if [ $? -ne 0 ]; then
+scp -P $ssh_port $build_tarball $user_name@$server_addr:$build_tarball
+
+## Check to see if that failed.
+if [ $? -ne 0 ]; then failure; fi
+
+
+## The scary part! Move the staging directory to the deploy directory. There
+## are quite a few ways of doing this so that it's an atomic operation, but
+## this is the simplest and serves our purposes.
+danger() {
   echo
   echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Danger !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
   echo '!                 The site could be in an unstable state              !'
   echo
   failure
-fi
+}
+trap danger SIGHUP SIGINT SIGTERM
 
+echo
+echo 'Deploying...'
+ssh -p $ssh_port $user_name@$server_addr \
+  "tar -xf $build_tarball && \
+   rm -r $deploy_dir && \
+   mv build $deploy_dir && \
+   chmod -R o-w,g+w $deploy_dir"
+
+if [ $? -ne 0 ]; then danger; fi
+
+## No need to trap signals anymore
+trap - SIGHUP SIGINT SIGTERM
+
+clean_up
+
+echo
+echo '*************************************************************************'
 echo 'Congrats! Deployment succeeded!'
